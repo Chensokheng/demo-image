02f445dfeba8905b9f3029ef9d41d55c48fb32a1
Chensokheng <chensokheng6@gmail.com>
2020-10-17 16:05:56 +0700

    :tada:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LazyLoadImage
+        src={process.env.PUBLIC_URL + 'lagre.jpg'}
+        alt="landscape"
+        effect="blur"
+        width="100%"
+        height="100%"
+        placeholderSrc={process.env.PUBLIC_URL + 'img.jpg'}
+      />
     </div>
   );
 }
